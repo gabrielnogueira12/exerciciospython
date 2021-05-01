ae48fdd61d9ee37054c9e1c69e8bd29a313a155c
Gabriel Nogueira <gabriel.nogueira142@gmail.com>
2021-05-01 20:30:51 -0300

    Um sistema simples de login. Você pode cadastrar um usuário, e logo após fazer login. O sistema verifica se o usuário está cadastrado e confirma ou não o login. Se não for verificado o login, ele pede pra você tentar novamente, tendo limite de até 3 vezes o erro.

---
+++ sistema-de-login.py
@@ -0,0 +1,39 @@
+def verification(user, password, users):
+    for count in range(0, len(users)):
+        if user == users[count]['Username'] and password == users[count]['Password']:
+            return True
+    return False
+
+
+id_user = {'Username': '', 'Password': ''}
+users = []
+
+menu = True
+
+while menu:
+    menu = input('Escolha sua opção:\n1. Cadastrar novo usuário\n2. Fazer login\n3. Sair\n')
+
+    if menu == '1':
+        id_user['Username'] = input('Digite o seu username: ')
+        id_user['Password'] = input('Digite sua senha: ')
+        users.append(id_user)
+
+    elif menu == '2':
+        username_login = input('Digite seu username: ')
+        password_login = input('Digite sua senha: ')
+        verif = verification(username_login, password_login, users)
+        error = 0
+        while not verif and error < 2:
+            error = error + 1
+            print('Usuário e/ou senha inválidos! Faça o login novamente')
+            username_login = input('Digite seu username: ')
+            password_login = input('Digite sua senha: ')
+            verif = verification(username_login, password_login, users)
+        if error == 2:
+            print('Você excedeu o número de tentativas! Tente novamente mais tarde.')
+        else:
+            print('Login bem sucedido!')
+        menu = False
+
+    else:
+        menu = False
