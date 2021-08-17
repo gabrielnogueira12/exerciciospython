9e5803d64ef0877109c80759506078cb0e64db95
Gabriel <gabriel.nogueira142@gmail.com>
2021-08-17 19:12:16 -0300

    Removing

--- lol_project/helper/leagues.py
+++
@@ -1,5 +0,0 @@
-from lol_project.datasets.read_file import df
-
-ligas = set()
-for item in df['League'].items():
-    ligas.add(item[1])
--- lol_project/test.py
+++
@@ -1,6 +0,0 @@
-from lol_project.functions.functions import adjust_df, show_match
-
-chosen_df = adjust_df('CBLoL', 1)
-print(chosen_df)
-
-# show_match(chosen_df[1])
